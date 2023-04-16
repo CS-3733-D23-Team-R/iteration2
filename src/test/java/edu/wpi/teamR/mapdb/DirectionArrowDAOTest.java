feab673297d0df2c28818c853cecd6ab705f1c0e
altonlm <121144911+altonlm@users.noreply.github.com>
2023-04-16 16:10:15 -0400

    Created test file

---
+++ src/test/java/edu/wpi/teamR/mapdb/DirectionArrowDAOTest.java
@@ -0,0 +1,32 @@
+package edu.wpi.teamR.mapdb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectionArrowDAOTest {
+
+    @Test
+    void addDirectionArrow() {
+    }
+
+    @Test
+    void deleteDirectionArrowByLongname() {
+    }
+
+    @Test
+    void deleteDirectionArrowsByKiosk() {
+    }
+
+    @Test
+    void deleteAllDirectionArrows() {
+    }
+
+    @Test
+    void getDirectionArrows() {
+    }
+
+    @Test
+    void getDirectionArrowsByKiosk() {
+    }
+}
